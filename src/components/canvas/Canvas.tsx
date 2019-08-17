5b85e19438ba81c5c84a0ae2e25519428c3aa2d8
jvii <jani.viitala1@gmail.com>
2019-08-17 08:24:59 +0300

    Move canvas hooks to hooks.tsx

--- src/components/canvas/Canvas.tsx
+++ src/components/canvas/Canvas.tsx
@@ -1,9 +1,9 @@
-import React, { useRef, useEffect, useReducer, useState, useMemo } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { CanvasState, CanvasStateAction } from './CanvasState';
 import { ToolbarState } from '../toolbar/ToolbarState';
 import { PaletteState } from '../palette/PaletteState';
-import { ToolState, toolStateReducer, Action } from '../../tools/ToolState';
-import { ZoomInitialPointSelectorTool } from '../../tools/ZoomInitialPointSelectorTool';
+import { ToolState, toolStateReducer } from '../../tools/ToolState';
+import { useSyncToTargetCanvas, useCanvasRef, useZoomToolInitialSelection } from './hooks';
 import './Canvas.css';
 import { Tool } from '../../tools/Tool';
 
@@ -91,86 +91,6 @@ function getEventHandler(
 
 function hasKey<O>(obj: O, key: keyof any): key is keyof O {
   return key in obj;
-}
-
-function useCanvasRef(
-  canvasDispatch: React.Dispatch<CanvasStateAction>,
-  isZoomCanvas: boolean
-): [React.RefObject<HTMLCanvasElement>] {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  useEffect((): void => {
-    if (!canvasRef.current) {
-      return;
-    }
-    canvasDispatch({
-      type: isZoomCanvas ? 'setZoomCanvasRef' : 'setMainCanvasRef',
-      canvas: canvasRef.current,
-    });
-  }, [canvasRef, canvasDispatch, isZoomCanvas]);
-  return [canvasRef];
-}
-
-function useSyncToTargetCanvas(
-  isZoomCanvas: boolean,
-  toolbarState: ToolbarState,
-  canvasState: CanvasState,
-  sourceCanvas: HTMLCanvasElement | null
-): React.Dispatch<React.SetStateAction<number>>[] {
-  const [syncPoint, setSyncPoint] = useState(0);
-  const syncTargetCanvas = isZoomCanvas ? canvasState.mainCanvasRef : canvasState.zoomCanvasRef;
-  const syncTargetCanvasContext = useMemo((): CanvasRenderingContext2D | null => {
-    if (syncTargetCanvas === null) {
-      return null;
-    }
-    return syncTargetCanvas.getContext('2d');
-  }, [syncTargetCanvas]);
-  useEffect((): void => {
-    if (!toolbarState.zoomModeOn) {
-      return;
-    }
-    syncToCanvas(syncTargetCanvasContext, sourceCanvas);
-  }, [syncPoint, toolbarState.zoomModeOn]); // sync if syncPoint set or zoomMode activated
-  return [setSyncPoint];
-}
-
-function syncToCanvas(
-  targetCanvasContext: CanvasRenderingContext2D | null,
-  sourceCanvas: HTMLCanvasElement | null
-): void {
-  if (targetCanvasContext && sourceCanvas) {
-    targetCanvasContext.drawImage(sourceCanvas, 0, 0);
-  }
-}
-
-function useZoomToolInitialSelection(
-  isZoomCanvas: boolean,
-  toolbarState: ToolbarState,
-  canvasDispatch: React.Dispatch<CanvasStateAction>,
-  toolState: ToolState,
-  toolStateDispatch: React.Dispatch<Action>
-): void {
-  useEffect((): void => {
-    if (isZoomCanvas) {
-      return;
-    }
-    // switch active tool to zoomInitialPointSelection for next render cycle
-    if (toolbarState.zoomModeOn) {
-      toolStateDispatch({ type: 'setActiveTool', tool: new ZoomInitialPointSelectorTool() });
-    } else {
-      canvasDispatch({
-        type: 'setZoomFocusPoint',
-        point: null,
-      });
-    }
-  }, [toolbarState.zoomModeOn]);
-
-  useEffect((): void => {
-    canvasDispatch({
-      type: 'setZoomFocusPoint',
-      point: toolState.zoomToolState.zoomInitialPoint,
-    });
-    toolStateDispatch({ type: 'setActiveTool', tool: toolbarState.selectedTool });
-  }, [toolState.zoomToolState.zoomInitialPoint]);
 }
 
 export default Canvas;
