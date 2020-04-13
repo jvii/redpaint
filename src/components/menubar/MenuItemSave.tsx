6e6b4482c00269b88554b1dd723b89ff20426eca
jvii <jani.viitala1@gmail.com>
2020-04-13 13:39:45 +0300

    MenuItem refactors

--- src/components/menubar/MenuItemSave.tsx
+++ src/components/menubar/MenuItemSave.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
 import './Menubar.css';
-import { useOvermind } from '../../overmind';
 
 interface Props {
   label: string;
+  objectURLToSave: string;
 }
 
-export function MenuItemSave({ label }: Props): JSX.Element {
-  const { state } = useOvermind();
+export function MenuItemSave({ label, objectURLToSave: href }: Props): JSX.Element {
   return (
     <div className="menu__item">
       <div className="menu__item-is-selected"></div>
-      <a
-        href={
-          state.undo.currentBufferItem ? URL.createObjectURL(state.undo.currentBufferItem) : '#'
-        }
-        download="testfile.png"
-        className="menu__item-label"
-      >
+      <a href={href} download="testfile.png" className="menu__item-label">
         {label}
       </a>
     </div>
