0a0171d11b4b780837a70e9a0446363de0e214df
jvii <jani.viitala1@gmail.com>
2021-05-23 21:24:46 +0300

    Separate Modal and Dialog components, BEMify styles

--- src/components/paletteEditor/PaletteEditor.tsx
+++ src/components/paletteEditor/PaletteEditor.tsx
@@ -1,16 +1,15 @@
-import React, { useRef } from 'react';
-import { Button, Divider, Slider, Typography } from '@material-ui/core';
+import React from 'react';
+import { Button, Divider, Slider } from '@material-ui/core';
 import './PaletteEditor.css';
 import { useOvermind } from '../../overmind';
 import { Color } from '../../types';
 import { paintingCanvasController } from '../../canvas/paintingCanvas/PaintingCanvasController';
 import { overlayCanvasController } from '../../canvas/overlayCanvas/OverlayCanvasController';
 import Palette from '../palette/Palette';
+import { Modal } from '../modal/Modal';
 
 export function PaletteEditor(): JSX.Element | null {
   const { state, actions } = useOvermind();
-
-  const overlayRef = useRef<HTMLDivElement>(document.createElement('div'));
 
   if (!state.paletteEditor.isOpen) {
     return null;
@@ -62,54 +61,49 @@ export function PaletteEditor(): JSX.Element | null {
 
   return (
     <>
-      <div className="modal-overlay-invisible" ref={overlayRef}>
-        <div className="palette-editor-modal">
-          <div className="modal-header">
-            <p>Color Palette</p>
+      <Modal header="Color palette">
+        <div className="palette-editor__container">
+          <div className="palette-editor__sliders">
+            <Slider
+              defaultValue={state.palette.foregroundColor.r}
+              step={1}
+              min={0}
+              max={255}
+              valueLabelDisplay="auto"
+              orientation="vertical"
+              onChange={(event, value) => setR(Number(value))}
+            />
+            <Slider
+              defaultValue={state.palette.foregroundColor.g}
+              step={1}
+              min={0}
+              max={255}
+              valueLabelDisplay="auto"
+              orientation="vertical"
+              onChange={(event, value) => setG(Number(value))}
+            />
+            <Slider
+              defaultValue={state.palette.foregroundColor.b}
+              step={1}
+              min={0}
+              max={255}
+              valueLabelDisplay="auto"
+              orientation="vertical"
+              onChange={(event, value) => setB(Number(value))}
+            />
           </div>
-          <Divider variant="middle" />
-          <div className="palette-editor-elements">
-            <div className="sliders">
-              <Slider
-                defaultValue={state.palette.foregroundColor.r}
-                step={1}
-                min={0}
-                max={255}
-                valueLabelDisplay="auto"
-                orientation="vertical"
-                onChange={(event, value) => setR(Number(value))}
-              />
-              <Slider
-                defaultValue={state.palette.foregroundColor.g}
-                step={1}
-                min={0}
-                max={255}
-                valueLabelDisplay="auto"
-                orientation="vertical"
-                onChange={(event, value) => setG(Number(value))}
-              />
-              <Slider
-                defaultValue={state.palette.foregroundColor.b}
-                step={1}
-                min={0}
-                max={255}
-                valueLabelDisplay="auto"
-                orientation="vertical"
-                onChange={(event, value) => setB(Number(value))}
-              />
-            </div>
-            <div className="palette-container">
-              <Palette></Palette>
-            </div>
+          <div className="palette-editor__palette-container">
+            <Palette></Palette>
           </div>
-          <Button variant="contained" color="primary" onClick={actions.paletteEditor.close}>
-            OK
-          </Button>
-          <Button variant="contained" color="secondary" onClick={actions.paletteEditor.close}>
-            Cancel
-          </Button>
         </div>
-      </div>
+        <Divider variant="middle" />
+        <Button variant="outlined" color="secondary" onClick={actions.paletteEditor.close}>
+          Cancel
+        </Button>
+        <Button variant="contained" color="primary" onClick={actions.paletteEditor.close}>
+          OK
+        </Button>
+      </Modal>
     </>
   );
 }
