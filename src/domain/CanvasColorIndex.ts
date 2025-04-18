71c1051d4c11d43d1090946bb7f3009867b9cbe2
jvii <jani.viitala1@gmail.com>
2025-04-18 09:41:41 +0300

    Commit old changes

--- src/domain/CanvasColorIndex.ts
+++ src/domain/CanvasColorIndex.ts
@@ -3,6 +3,9 @@ import { Point } from '../types';
 export class CanvasColorIndex {
   width: number;
   height: number;
+  // Internally we store the color index as a typed array.
+  // This is because we use the color indes as a webgl texture.
+  // Each pixel is represented by 4 components (corresponding to r,g,b,a)
   indexArray: Uint8Array;
 
   constructor(width: number, height: number, indexArray: Uint8Array) {
@@ -16,32 +19,28 @@ export class CanvasColorIndex {
     height: number,
     backgroundColorNumber: number
   ): CanvasColorIndex {
-    // initilize an array with all zeros
-    const arrayLength = width * height * 4;
+    const stride = 4;
+    const arrayLength = width * height * stride;
     const indexArray = new Uint8Array(arrayLength).fill(0);
 
     // Give each pixel an initial color number (the background color)
     // We use the R component to store the color number
-    const initial = 0;
-    const stride = 4;
-    for (let i = initial; i < arrayLength; i = i + stride) {
+
+    for (let i = 0; i < arrayLength; i = i + stride) {
       indexArray[i] = backgroundColorNumber;
     }
+
     return new CanvasColorIndex(width, height, indexArray);
   }
 
-  getColorNumberForPixel(pixel: Point, height: number, width: number): number {
-    const arrayIndex = pixel.x * 4 + (height - pixel.y - 1) * width * 4;
+  getColorNumberForPixel(pixel: Point): number {
+    const arrayIndex = pixel.x * 4 + (this.height - pixel.y - 1) * this.width * 4;
     return this.indexArray[arrayIndex];
   }
 
-  setColorNumberForPixel(
-    pixel: Point,
-    colorNumberForPixel: number,
-    height: number,
-    width: number
-  ): void {
-    const arrayIndex = pixel.x * 4 + (height - pixel.y - 1) * width * 4;
+  // Used by floodfill
+  setColorNumberForPixel(pixel: Point, colorNumberForPixel: number): void {
+    const arrayIndex = pixel.x * 4 + (this.height - pixel.y - 1) * this.width * 4;
     this.indexArray[arrayIndex] = colorNumberForPixel;
   }
 }
